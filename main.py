8f16132f933fcb76c04d066c3aff503951caf419
SPG2001 <spgabel2@mail.fhsu.edu>
2023-09-24 13:58:12 -0500

    Removed experimental code. Pandas frame with names can be developed using Faker. Added requirements.txt

--- main.py
+++ main.py
@@ -1,44 +1,34 @@
+# INF601 - Advanced Programming in Python
+# Stephen Gabel
+# Mini Project 2
+
+
 from faker import Faker
 import numpy as np
 import matplotlib as plt
 import pandas as pd
 
-fakeData = []
 fake = Faker()
 
-
+#Create the data frame that contains the first and last names.
 def fakeFrameMaker():
+
+    fake = Faker()
+
+    global fakerFrame
+    fakeFirstNames = []
+    fakeLastNames = []
+
+    for _ in range(100):
+        fakeFirstNames.append(fake.name())
+        fakeLastNames.append(fake.name())
+
     fakerFrame = pd.DataFrame(
         {
-            "Name": [
-
-            ],
-            "Age": [],
-            "Sex": []
+            "First Name": fakeFirstNames,
+            "Last Name": fakeLastNames
         }
     )
-
-    fakeData = []
-
-    fakeName = Faker()
-    fakeAge = Faker()
-    fakeSex = Faker()
-
-    #for _ in range(100):
-
-
-
-
-print(fakeData)
-
-fakerFrame = pd.DataFrame(
-    {
-        "Name": ["Stephen"
-
-                 ],
-        "Age": ["22"],
-        "Sex": ["Male"]
-    }
-)
+fakeFrameMaker()
 
 print(fakerFrame)
